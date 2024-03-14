9933161fe9b6c63f93b0d6598262ffe4bd8d9cc5
Nordstroem1 <johannord27@hotmail.com>
2024-03-14 20:20:31 +0100

    Fixade om quick sort algoritmen, den sorterar nu objekt istället för att sortera pagecount enbart.

--- InlämningOODesign/Commands/SortingAlgorithms/QuickSort.cs
+++ InlämningOODesign/Commands/SortingAlgorithms/QuickSort.cs
@@ -14,66 +14,67 @@ namespace InlämningOODesign.Commands.SortingAlgorithms
     public class QuickSort : ICommand
     {
         WannabeDatabse db = WannabeDatabse.Instance;
+        List<Book> list = new List<Book>();
         public void Command()
         {
             Logic();
         }
         public void Logic()
         {
-            int[] sortedArray = FillArrayFromDatabase();
-            Sort(sortedArray,0,sortedArray.Length -1);
-
+            FillArrayFromDatabase();
+            Sort(list,0,list.Count -1);
+            Console.Clear();
             Console.WriteLine($"The books amount of pages in acending order:");
-            foreach (int num in sortedArray)
+            foreach (Book book in list)
             {
-                Console.WriteLine(num);
+                Console.WriteLine($"{book.PageCount}\t{book.Title}");
             }
         }
 
-        private void Sort(int[] pages, int start, int end)
+        private void Sort(List<Book> list, int start, int end)
         {
             if(start < end)
             {
-                int pivot = Partition(pages, start, end);
-                Sort(pages, start, pivot -1);
-                Sort(pages,pivot +1, end);
+                int pivot = Partition(list, start, end);
+                Sort(list, start, pivot -1);
+                Sort(list,pivot +1, end);
             }
         }
 
-        private int Partition(int[] array, int start, int end)
+        private int Partition(List<Book> list, int start, int end)
         {
-            int pivot = array[end];
+            int pivot = list[end].PageCount;
             int i = start;
 
             for(int j = start; j < end; j++)
             {
-                if (array[j] < pivot) 
+                if (list[j].PageCount < pivot) 
                 {
-                    swap(array,i,j);
+                    swap(list,i,j);
                     i++;
                 }
             }
-            swap(array,i,end);
+            swap(list,i,end);
             return i;
         }
 
-        private void swap(int[] array, int i, int j)
+        private void swap(List<Book> list, int i, int j)
         {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            Book temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
         }
 
-        private int[] FillArrayFromDatabase()
+        private List<Book> FillArrayFromDatabase()
         {
-            int[] pagesFromBooks = new int[db.books.Count];
-            int index = 0;
             foreach (Book book in db.books.Values)
             {
-                pagesFromBooks[index] = book.PageCount;
-                index++;
+                if (!list.Contains(book))
+                {
+                    list.Add(book);
+                }
             }
-            return pagesFromBooks;
+            return list;
         }
     }
 }
